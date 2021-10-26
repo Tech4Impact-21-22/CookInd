ac19ca2e13f73bb1337dead107233188dfc5a56c
AurelliaChristie <aurelliachristie77@gmail.com>
2021-10-26 15:26:53 +0700

    Finish page ubah password

---
+++ js/ubah-password.js
@@ -0,0 +1,56 @@
+// Get HTML elements
+const elEmail = document.querySelector('#email');
+const elPassword = document.querySelector('#password');
+const elConfirmPassword = document.querySelector('#conf-password');
+const elBtn = document.querySelector('#submit');
+const elInvalidEmail = document.querySelector('#email-invalid');
+const elInvalidPassword = document.querySelector('#pass-invalid');
+const elInvalidConfirmPassword = document.querySelector('#conf-pass-invalid');
+const elForm = document.querySelector('form');
+
+let passValidation = new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/);
+
+elBtn.addEventListener('click', function(e){
+    e.preventDefault();
+    if(elInvalidEmail.classList[1] !== 'd-none'){
+        elInvalidEmail.classList.add('d-none');
+    }
+    if(elInvalidPassword.classList[1] !== 'd-none'){
+        elInvalidPassword.classList.add('d-none');
+    }
+    if(elInvalidConfirmPassword.classList[1] !== 'd-none'){
+        elInvalidConfirmPassword.classList.add('d-none')
+    }
+    let email = elEmail.value;
+    let password = elPassword.value;
+    let confirmPassword = elConfirmPassword.value;
+    let signedUp = JSON.parse(localStorage.getItem(email));
+    if(email!==''){
+        if(password !=='' && passValidation.test(password)){
+            if(signedUp){
+                if(password===confirmPassword){
+                    localStorage.setItem(email, JSON.stringify({'name':signedUp.name, 'pass': password}));
+                    window.history.back();
+                    elForm.reset();
+                }else{
+                    elConfirmPassword.value = '';
+                    elInvalidConfirmPassword.classList.remove('d-none');
+                }
+            }else{
+                alert('Email yang anda masukkan belum terdaftar.');
+                elForm.reset();
+            }
+        }else{
+            elPassword.value = '';
+            elConfirmPassword.value = '';
+            elInvalidPassword.classList.remove('d-none');
+        }
+        
+    }else{
+        elEmail.value = '';
+        elPassword.value = '';
+        elConfirmPassword.value = '';
+        elInvalidEmail.classList.remove('d-none');
+    }
+    
+})
